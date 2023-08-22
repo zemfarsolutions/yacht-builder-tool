bf9ac955b8ab759b4257862bd57db31f801c9ab2
developerzemfar <developerzemfar@gmail.com>
2023-08-22 06:50:38 +0500

    Builder Tool

---
+++ src/components/Builder.vue
@@ -0,0 +1,113 @@
+<script setup>
+import { computed, ref } from 'vue';
+import VueDragResize from 'vue3-drag-resize'
+const width = ref(0)
+const height = ref(0)
+const top = ref(0)
+const left = ref(0)
+const num = ref(0)
+
+
+const rotate = computed(function() {
+    return `rotate(${num.value}deg)`
+})
+
+function dragResize(newRect) {
+    console.log(newRect)
+    width.value = newRect.width
+    height.value = newRect.height
+    top.value = newRect.top
+    left.value = newRect.left
+}
+
+function onRotate() {
+    num.value++
+}
+
+const images = ref([
+  {
+    name: 'image39.png'
+  },
+  {
+    name: 'image40.png'
+  },
+  {
+    name: 'image41.png'
+  },
+  {
+    name: 'image43.png'
+  },
+  {
+    name: 'image44.png'
+  },
+  {
+    name: 'image45.png'
+  }
+]);
+</script>
+
+<template>
+    
+    <div class="container">
+        <div class="d-flex">
+            
+            <div class="aside">
+                <div class="row">
+                    <h5 class="fw-bold">Tools:</h5>
+                    
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6" v-for="image in images">
+                        <VueDragResize
+                        :is-active="false"
+                        :w="200"
+                        :h="200"
+                        contentClass="transform-rotate"
+                        class="bg-primary-100 dark:bg-gray-800"
+                        @resizing="dragResize"
+                        @dragging="dragResize"
+                        >
+                            <img :src="`media/`+image.name" alt="" class="w-50">
+                        </VueDragResize>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="content">
+                <div class="canvas">
+                            <img src="/media/image28.png" alt="" style="width: 400px">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+.aside {
+    top: 0;
+    left: 0;
+    width: 250px;
+    height: 500px;
+}
+
+.content {
+    height: 500px;
+    width: 1000px;
+    padding: 30px;
+    background-color: skyblue;
+}
+
+.canvas {
+    margin-top: 150px;
+    display: flex;
+    justify-content: center;
+    
+}
+
+.transform-rotate{
+    transform: v-bind(rotate)
+}
+</style>
