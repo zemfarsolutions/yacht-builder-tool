0dc866bacd149ee6f530288c05f6f53e2ee40722
developerzemfar <developerzemfar@gmail.com>
2023-08-29 07:03:17 +0500

    Tool List & Selection.

--- src/components/Builder.vue
+++ src/components/Builder.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { computed, ref } from 'vue';
 import VueDragResize from 'vue3-drag-resize'
+
 const width = ref(0)
 const height = ref(0)
 const top = ref(0)
 const left = ref(0)
 const num = ref(0)
-
+const checkImages = ref([]);
 
 const rotate = computed(function() {
     return `rotate(${num.value}deg)`
@@ -20,13 +21,13 @@ function dragResize(newRect) {
     left.value = newRect.left
 }
 
-function onRotate() {
-    num.value++
+function check(e) {
+    console.log(checkImages.value);
 }
 
 const images = [];
 for (let index = 0; index < 86; index++) {
-    if (index != 28) {
+    if (index != 28 && index != 0) {
         const element = 'image'+index+'.png';
         images.push(element) 
     }
@@ -39,19 +40,47 @@ const items = ref(images);
 <template>
     
     <div class="container">
-        <div class="d-flex">
-            
-            <div class="row">
-                <h5 class="fw-bold">Tools:</h5>
-            </div>
-            <div class="aside" >
+        <div class="row">
 
+            <div class="col-md-2">
                 <div class="row">
-                    <div class="col-md-6" v-for="image in items">
+                    <div class="col-md-12 col-sm-12 col-lg-12">
+                        <h5 class="fw-bold">Select Tools:</h5>
+                    </div>
+                </div>
+
+                <div class="row" style="overflow-y: scroll;overflow-x: hidden; height: 500px">
+                    
+                    <div class="col-md-6 image-container" v-for="(image, index) in items" :key="index">
+                        
+                        <input type="checkbox" 
+                        :id="`dessert-`+index" 
+                        v-model="checkImages"
+                        @change="check($event)"/>
+
+                        <label :for="`dessert-`+index">
+                            <img :src="`media/`+image" />
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-lg-12">
+                        <h5 class="fw-bold">Tools:</h5>
+                    </div>
+                </div>
+
+                <div class="row"> 
+                    <div class="col-md-6" v-for="(image, index) in items" :key="index">
+                        
                         <VueDragResize
                         :is-active="false"
-                        :w="200"
-                        :h="140"
+                        :w="100"
+                        :h="90"
+                        v-if="image == 'image1.png'"
                         style="position: relative;"
                         class="bg-primary-100 dark:bg-gray-800"
                         @resizing="dragResize"
@@ -59,13 +88,16 @@ const items = ref(images);
                         >
                             <img :src="`media/`+image" alt="" class="w-50">
                         </VueDragResize>
+
                     </div>
                 </div>
             </div>
-    
-            <div class="content">
-                <div class="canvas">
-                    <img src="/media/image28.png" alt="" style="width: 400px">
+           
+            <div class="col-md-8">
+                <div class="content">
+                    <div class="canvas">
+                        <img src="/media/image28.png" alt="" style="width: 400px">
+                    </div>
                 </div>
             </div>
         </div>
@@ -74,6 +106,50 @@ const items = ref(images);
 </template>
 
 <style scoped>
+
+.image-container {
+    height: 100px;
+    background-color: #ffffff;
+    box-shadow: 0 0 25px rgba(17, 1, 68, 0.08);
+    border-radius: 8px;
+    position: relative;
+    cursor: pointer;
+    margin-bottom: 10px;
+}
+
+.image-container img {
+    width: 70%;
+    position: absolute;
+    margin: auto;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    cursor: pointer;
+}
+
+input[type="checkbox"] {
+  -webkit-appearance: none;
+  position: relative;
+  width: 100%;
+  cursor: pointer;
+}
+input[type="checkbox"]:after {
+  position: relative;
+  font-family: "Font Awesome 5 Free";
+  font-weight: 400;
+  content: "\f111";
+  font-size: 18px;
+  color: #478bfb;
+  z-index: 999;
+  right: 5px;
+  top: -1px;
+}
+input[type="checkbox"]:checked:after {
+  font-weight: 900;
+  content: "\f058";
+  color: #478bfb;
+}
 
 .aside {
     top: 0;
@@ -99,5 +175,24 @@ const items = ref(images);
 
 .transform-rotate{
     transform: v-bind(rotate)
+}
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: skyblue;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import VueSelectImage from 'vue-select-image'
+// add stylesheet
+import 'vue-select-image/dist/vue-select-image.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueSelectImage)
 
 app.mount('#app')
