93c0c73bd91aa1b653209c47b6b7d80abff857f6
developerzemfar <developerzemfar@gmail.com>
2023-09-05 07:07:28 +0500

    UI Design Updated

--- src/components/Builder.vue
+++ src/components/Builder.vue
@@ -6,18 +6,6 @@ const width = ref(0)
 const height = ref(0)
 const top = ref(0)
 const left = ref(0)
-const num = ref(0)
-const checkImages = ref([{
-        path: 'image1.png'
-    }]);
-const checkedImages = ref([]);
-const result = ref([{
-        path: 'image1.png'
-    }]);
-
-const rotate = computed(function() {
-    return `rotate(${num.value}deg)`
-})
 
 function dragResize(newRect) {
     width.value = newRect.width
@@ -25,48 +13,6 @@ function dragResize(newRect) {
     top.value = newRect.top
     left.value = newRect.left
 }
-
-function check(e) {
-    
-    for (let index = 0; index < checkImages.value.length; index++) {
-
-        const check = checkedImages.value.includes(checkImages.value[index]);
-        
-        if (!check) {
-
-            const obj = {
-                path: checkImages.value[index]
-            }
-
-            checkedImages.value.push(obj)
-
-            var props = ['path'];
-        
-            console.log(checkedImages.value)
-
-            result.value = checkImages.value.filter(function(o1){
-
-                // filter out (!) items in result2
-                return !checkedImages.value.some(function(o2){
-                    return o1.path === o2.path;          // assumes unique id
-                });
-
-            }).map(function(o){
-
-                // use reduce to make objects with only the required properties
-                // and map to apply this to the filtered array as a whole
-                return props.reduce(function(newo, path){
-                    newo[path] = o[path];
-                    return newo;
-                }, {});
-
-            });
-        }
-    } 
-
-    console.log(result.value)
-}
-
 
 const images = [];
 
@@ -84,144 +30,281 @@ for (let index = 0; index < 86; index++) {
 }
 
 const items = ref(images);
+const products = ref(true);
+const summary = ref(false);
+const currentTab = ref(0);
+
+function changeMenu(tab) {
+    if (tab == 'summary') {
+
+        products.value = false;
+        summary.value = true;
+        currentTab.value = 1;
+
+    }else{
+
+        products.value = true
+        summary.value = false
+        currentTab.value = 0;
+    }
+}
+
+const cardDetail = ref(false);
 
 </script>
 
 <template>
     
-    <div class="container">
-        <div class="row">
-
-            <div class="col-md-2">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-lg-12">
-                        <h5 class="fw-bold">Select Tools:</h5>
-                    </div>
-                </div>
-
-                <div class="row" style="overflow-y: scroll;overflow-x: hidden; height: 500px">
-                    
-                    <div class="col-md-6 image-container" v-for="(image, index) in items" :key="index">
-                        
-                        <input type="checkbox" 
-                        :id="`dessert-`+index"
-                        :value="image"
-                        :disabled="image.path == 'image1.png'"
-                        v-model="checkImages"
-                        @change="check($event)"/>
-
-                        <label :for="`dessert-`+index">
-                            <img :src="`media/`+image.path" />
-                        </label>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="col-md-2">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-lg-12">
-                        <h5 class="fw-bold">Tools:</h5>
-                    </div>
-                </div>
-
-                <div class="row"> 
-                    <div class="col-md-6" v-for="(image, index) in result" :key="index">
-                        
-                        <VueDragResize
-                        :is-active="false"
-                        :w="100"
-                        :h="90"
-                        style="position: relative;"
-                        class="bg-primary-100 dark:bg-gray-800"
-                        @resizing="dragResize"
-                        @dragging="dragResize"
-                        >
-                            <img :src="`media/`+image.path" alt="" class="w-50">
-                        </VueDragResize>
-
-                    </div>
-                </div>
-            </div>
-           
-            <div class="col-md-8">
-                <div class="content">
-                    <div class="canvas">
-                        <img src="/media/image28.png" alt="" style="width: 400px">
-                    </div>
-                </div>
-            </div>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Summary</a>
         </div>
+    </nav>
+
+    <div class="container">
+            
+        <!-- <div class="d-flex"> -->
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="content">
+                        <div class="canvas">
+                            <img src="/media/image28.png" alt="" style="width: 400px;">
+                        </div>
+                    </div>
+                    
+                    <transition name="fade">
+                        <div v-show="cardDetail" class="card mb-3 card-detail">
+                            <div class="row g-0">
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Risky Business 30</h5>
+                                        <ul style="list-style: none;">
+                                            <li style="font-size: 12px;">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                            <li style="font-size: 12px;">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                            <li style="font-size: 12px;">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="media/view.jpg" class="img-fluid rounded-start" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </transition>
+
+                </div>
+
+                <div class="col-md-3">
+                    <button @click="changeMenu('products')" :class="currentTab === 0 ? `btn btn-outline-light text-secondary mx-2 btn-tab btn-tab-active` : 'btn btn-outline-light text-secondary mx-2 btn-tab'">
+                        Products
+                    </button>
+                    <button @click="changeMenu('summary')" :class="currentTab === 1 ? `btn btn-outline-light text-secondary mx-2 btn-tab btn-tab-active` : 'btn btn-outline-light text-secondary mx-2 btn-tab'">
+                        Summary
+                    </button>
+                </div>
+                
+                <div v-show="products" class="row card-list">
+                    <div class="col-md-2">
+    
+                        <div 
+                        @mouseover="cardDetail = true" 
+                        @mouseleave="cardDetail = false" 
+                        class="card mt-2">
+                            <img src="media/view.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Risky Buisness 30</h5>
+                                <p class="card-text">30cm / 12 cm XYZ.</p>
+                            </div>
+                        </div>
+    
+                        <div 
+                        @mouseover="cardDetail = true" 
+                        @mouseleave="cardDetail = false" 
+                        class="card mt-2">
+                            <img src="media/view.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Risky Buisness 30</h5>
+                                <p class="card-text">30cm / 12 cm XYZ.</p>
+                            </div>
+                        </div>
+    
+                        <div 
+                        @mouseover="cardDetail = true" 
+                        @mouseleave="cardDetail = false" 
+                        class="card mt-2">
+                            <img src="media/view.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Risky Buisness 30</h5>
+                                <p class="card-text">30cm / 12 cm XYZ.</p>
+                            </div>
+                        </div>
+    
+                    </div>
+                    <div class="col-md-2">
+    
+                        <div 
+                        @mouseover="cardDetail = true" 
+                        @mouseleave="cardDetail = false" 
+                        class="card mt-2">
+                            <img src="media/view.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Risky Buisness 30</h5>
+                                <p class="card-text">30cm / 12 cm XYZ.</p>
+                            </div>
+                        </div>
+    
+                        <div 
+                        @mouseover="cardDetail = true" 
+                        @mouseleave="cardDetail = false" 
+                        class="card mt-2">
+                            <img src="media/view.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Risky Buisness 30</h5>
+                                <p class="card-text">30cm / 12 cm XYZ.</p>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+
+                <div v-show="summary" class="row card-list">
+                    <div class="col-md-4">
+                        <div class="card mt-2" style="width: 400px;box-shadow: none;">
+                            <div class="row g-0">
+                                <div class="col-md-12">
+                                    <div class="card-body">
+                                        <div class="mt-2" style="display: flow-root">
+                                            <p class="float-start fw-bold fs-6">FEATURE:</p>
+                                            <p class="float-end text-danger">1</p>
+                                        </div>
+                                        <div class="mt-2" style="display: flow-root">
+                                            <p class="float-start fw-bold fs-6">CAPACITY:</p>
+                                            <p class="float-end text-danger">10</p>
+                                        </div>
+                                        <div class="mt-2" style="display: flow-root">
+                                            <p class="float-start fw-bold fs-6">ANCHORS:</p>
+                                            <p class="float-end text-danger">3</p>
+                                        </div>
+                                        <div class="mt-2" style="display: flow-root">
+                                            <p class="float-start fw-bold fs-6">TOTAL COST:</p>
+                                            <p class="float-end text-danger">$ 5,135</p>
+                                        </div>
+
+                                        <hr>
+
+                                        <div class="card mb-3" style="width: 370px;">
+                                            <div class="row g-0">
+                                                <div class="col-md-4">
+                                                    <img src="media/view.jpg" class="img-fluid rounded-start" alt="...">
+                                                </div>
+                                                <div class="col-md-8">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Risky Buisness 30</h5>
+                                                        <p class="card-text">30' 0" - 10 - $ 5,995</p>
+                                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        <!-- </div> -->
+
     </div>
 
 </template>
 
 <style scoped>
 
-.image-container {
-    height: 100px;
-    background-color: #ffffff;
-    box-shadow: 0 0 25px rgba(17, 1, 68, 0.08);
-    border-radius: 8px;
-    position: relative;
-    cursor: pointer;
-    margin-bottom: 10px;
+.container {
+    padding: 0px;
+    margin-top: 5px;
+    margin: 0;
 }
 
-.image-container img {
-    width: 70%;
-    position: absolute;
-    margin: auto;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    cursor: pointer;
+.card {
+    width: 180px;
+    border: none;
+    box-shadow: 1px 1px 8px #a1a1a1;
+    position: sticky;
 }
 
-input[type="checkbox"] {
-  -webkit-appearance: none;
-  position: relative;
-  width: 100%;
-  cursor: pointer;
+.card-title {
+    font-size: 16px;
 }
-input[type="checkbox"]:after {
-  position: relative;
-  font-family: "Font Awesome 5 Free";
-  font-weight: 400;
-  content: "\f111";
-  font-size: 18px;
-  color: #478bfb;
-  z-index: 999;
-  right: 5px;
-  top: -1px;
-}
-input[type="checkbox"]:checked:after {
-  font-weight: 900;
-  content: "\f058";
-  color: #478bfb;
+.card-text {
+    font-size: 12px;
 }
 
-.aside {
-    top: 0;
-    left: 0;
-    width: 250px;
+.card-list {
+    justify-content: end;
+    overflow-y: scroll;
     height: 500px;
-    position: relative;
+    position: absolute;
+    right: 35px;
+    top: 100px;
 }
+
+.card-detail {
+    width: 500px;
+    font-family:sans-serif;
+    font-weight: bold;
+    font-size: 13px;
+    z-index: 9999;
+    right: 450px;
+    float: right;
+    margin-top: 315px;
+}
+
+.btn-tab {
+    border-bottom-left-radius: unset;
+    border-bottom-right-radius: unset;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+}
+
+.btn-tab-active {
+    border-bottom: 1px solid skyblue;
+}
+
+.vdr.active:before {
+    outline: 0px;
+}
+
+.fade-enter-from {
+    opacity: 0;
+    transform: translateY(-10px)
+}
+
+.fade-enter-to {
+    opacity: 0;
+    transform: translateY(0)
+}
+
+.fade-enter-active {
+    transition: all 0.1s ease;
+}
+
 
 .content {
     height: 500px;
-    width: 900px;
     padding: 30px;
     background-color: skyblue;
+    width: 830px;
+    position: absolute;
 }
 
 .canvas {
     margin-top: 150px;
     display: flex;
     justify-content: center;
-    
 }
 
 .transform-rotate{
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,62 @@ defineProps({
     required: true
   }
 })
+
+const num = ref(0)
+const checkImages = ref([{
+        path: 'image1.png'
+    }]);
+const checkedImages = ref([]);
+const result = ref([{
+        path: 'image1.png'
+    }]);
+
+const rotate = computed(function() {
+    return `rotate(${num.value}deg)`
+})
+
+
+function check(e) {
+    
+    for (let index = 0; index < checkImages.value.length; index++) {
+
+        const check = checkedImages.value.includes(checkImages.value[index]);
+        
+        if (!check) {
+
+            const obj = {
+                path: checkImages.value[index]
+            }
+
+            checkedImages.value.push(obj)
+
+            var props = ['path'];
+        
+            console.log(checkedImages.value)
+
+            result.value = checkImages.value.filter(function(o1){
+
+                // filter out (!) items in result2
+                return !checkedImages.value.some(function(o2){
+                    return o1.path === o2.path;          // assumes unique id
+                });
+
+            }).map(function(o){
+
+                // use reduce to make objects with only the required properties
+                // and map to apply this to the filtered array as a whole
+                return props.reduce(function(newo, path){
+                    newo[path] = o[path];
+                    return newo;
+                }, {});
+
+            });
+        }
+    } 
+
+    console.log(result.value)
+}
+
 </script>
 
 <template>
@@ -16,6 +72,32 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
+  
+            <div class="col-md-2">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-lg-12">
+                        <h5 class="fw-bold">Select Tools:</h5>
+                    </div>
+                </div>
+
+                <div class="row" style="overflow-y: scroll;overflow-x: hidden; height: 500px">
+                    
+                    <div class="col-md-6 image-container" v-for="(image, index) in items" :key="index">
+                        
+                        <input type="checkbox" 
+                        :id="`dessert-`+index"
+                        :value="image"
+                        :disabled="image.path == 'image1.png'"
+                        v-model="checkImages"
+                        @change="check($event)"/>
+
+                        <label :for="`dessert-`+index">
+                            <img :src="`media/`+image.path" />
+                        </label>
+                    </div>
+
+                </div>
+            </div>
 </template>
 
 <style scoped>
@@ -33,6 +115,51 @@ h3 {
 .greetings h1,
 .greetings h3 {
   text-align: center;
+}
+
+input[type="checkbox"] {
+  -webkit-appearance: none;
+  position: relative;
+  width: 100%;
+  cursor: pointer;
+}
+input[type="checkbox"]:after {
+  position: relative;
+  font-family: "Font Awesome 5 Free";
+  font-weight: 400;
+  content: "\f111";
+  font-size: 18px;
+  color: #478bfb;
+  z-index: 999;
+  right: 5px;
+  top: -1px;
+}
+input[type="checkbox"]:checked:after {
+  font-weight: 900;
+  content: "\f058";
+  color: #478bfb;
+}
+
+
+.image-container {
+    height: 100px;
+    background-color: #ffffff;
+    box-shadow: 0 0 25px rgba(17, 1, 68, 0.08);
+    border-radius: 8px;
+    position: relative;
+    cursor: pointer;
+    margin-bottom: 10px;
+}
+
+.image-container img {
+    width: 70%;
+    position: absolute;
+    margin: auto;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    cursor: pointer;
 }
 
 @media (min-width: 1024px) {
